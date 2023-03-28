a90fce9f05c811a4ed8f9e08ee3fdc755d221f53
Poonam <poonam.trifed@gmail.com>
2023-03-28 10:50:52 +0530

    refresh

--- src/Components/homepage/homepage.jsx
+++ src/Components/homepage/homepage.jsx
@@ -1,107 +1,110 @@
-import React from 'react'
-import { useEffect, useState} from 'react';
-import '../homepage/home.css';
-import Button from 'react-bootstrap/Button'
-import Modal from 'react-bootstrap/Modal'
+import { useEffect, useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import '../homepage/home.css'
 
-const Homepage = () => {
-    const [list, setList] = useState([]);
+const LandingPage = () => {
+
+    const [allBrandList, setAllBrandList] = useState([]);
     const [searchText, setSearchText] = useState("");
-    const [searchType , setSearchType] = useState("");
-    const [ownerDetail , setOwnerDetail] = useState({});
-    const [show, setShow] = useState(false)
-   
+    const [searchType, setSearchType] = useState("");
+    const [ownerDetail, setOwner] = useState({});
+    const [show, setShow] = useState(false);
 
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-    const handleChange = ()=> setShow(false);
-    const handleShow = ()=> setShow(true);
-
-    const handleOwn = (owner)=>{
-        setOwnerDetail(owner);
+    const handleOwner = (owner) => {
+        setOwner(owner);
         handleShow();
-    }
-    useEffect(()=>{
-         let API = 'https://vpic.nhtsa.dot.gov/api/vehicles/getmanufacturerdetails/honda?format=json';
+    };
 
-         if(searchText){
-            API = `https://vpic.nhtsa.dot.gov/api/vehicles/getmanufacturerdetails/${searchText}honda?format=json`
-         }
+    useEffect(() => {
+        let url = "https://vpic.nhtsa.dot.gov/api/vehicles/getmanufacturerdetails/honda?format=json";
 
-        fetch(API).then(response => response.json()).then(data =>{
-            setList([...data.Results]);
-            if(searchType){
-                let filter = list.filter((item)=>item?.VehicleType?.[0]?.Name === searchType);
-                setList(filter);
-            }
-        }).catch(error =>{
-            console.error(error);
-        })
-    }, [searchText, searchType]);
-
-  return (
-    <>
-      <h1 className='heading'>Vehicle Manufacturer Catalog</h1>
-       <section className='home'>
-        <div className='search-bar1'>
-            <div className='search'>
-                <label htmlFor="searchByName">Search By Name</label>
-                <input type="text" name="search" id="search" value={searchText} onChange={(e)=>setSearchText(e.target.value)} />
-
-            </div>
-            <div className='search-bar2'>
-                <label htmlFor="searchBytype" >Search By Type</label>
-                <select className="searchType" id="searchType" value={searchType} onChange={(e)=>setSearchType(e.target.value)}> 
-                    <option value="">All</option>
-                    <option value="Motorcycle">Motorcycle</option>
-                    <option value="LMV">LMV</option>
-                    <option value="Passenger">Passenger</option>
-                    <option value="Bus">Bus</option>
-                    <option value="Truck">Truck</option>
-                </select>
-            </div>
-        </div>
-
-
-        <div className='context'>
-            <div className='car'>Name</div>
-            <div className='country'>Country</div>
-            <div className='type'>Type</div>
-        </div>
-
-        {
-            list.map((item, i)=>{
-                return(
-                    <>
-                        {item.Mfr_CommonName && item.Country && item.VehicleTypes[0]?.Name &&
-                            <div className='data-box' key={i} onClick={()=> handleOwn(item)}>
-                                <div className='car'>{item.Mfr_CommonName}</div>
-                                <div className='country'>{item.Country}</div>
-                                <div className='type'>{item.VehicleTypes[0]?.Name}</div>
-                            </div>
-                        }
-                    
-                    </>
-                )
-            })
+        if (searchText) {
+            url = `https://vpic.nhtsa.dot.gov/api/vehicles/getmanufacturerdetails/${searchText}?format=json`;
         }
 
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                setAllBrandList([...data.Results]);
+                if (searchType) {
+                    let filteredData = allBrandList.filter((item) => item?.VehicleTypes?.[0]?.Name === searchType);
+                    setAllBrandList(filteredData);
+                }
 
-        <div>
-            <Modal className="popup" show={show} onHide={handleChange} centered>
-                <Modal.Body className="body">{ownerDetail.Country}
-                    <div className='ownerDetail'>{ownerDetail.Mfr_Name}</div>
-                    <div className='owner-ceo'>{ownerDetail.PricipalPositio}</div>
-                    <div className='address'>{ownerDetail.Address}</div>
-                </Modal.Body>
-                <Modal.Footer className="foote">
-                    <Button onClick={()=>handleChange()} variant="secondary">Close</Button>
-                </Modal.Footer>
-            </Modal>
-        </div>
-       </section>
-     
-    </>
-  )
+            })
+            .catch((error) => console.error(error));
+    }, [searchText, searchType]);
+
+    // console.log(filteredData)
+    return (
+        <>
+            <header>
+                <h1>VEHICLE MANUFACTURERS</h1>
+            </header>
+            <section className="landing-page-section">
+                <div className="search-container">
+                    <div className="search">
+                        <label htmlFor="searchByName">Search by Name</label>
+                        <input type="text" name="search" id="search" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+                    </div>
+                    <div>
+                        <label htmlFor="searchByType">Search by Type</label>
+                        <select className="search" id="searchByType" value={searchType} onChange={(e) => setSearchType(e.target.value)}>
+                            <option value="">All</option>
+                            <option value="Motorcycle">Motorcycle</option>
+                            <option value="LMV">LMV</option>
+                            <option value="Passenger Car">Passenger Car</option>
+                            <option value="Bus">Bus</option>
+                            <option value="Truck">Truck</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className="title-name-box">
+                    <div className="car-name">Name</div>
+                    <div className="country-name">Country</div>
+                    <div className="type-name">Type</div>
+                </div>
+
+                {allBrandList.map((item, i) => {
+                    return (
+                        <>
+                            {item.Mfr_CommonName && item.Country && item.VehicleTypes[0]?.Name &&
+                                <div className="title-name-box" key={i} onClick={() => handleOwner(item)}>
+                                    <div className="car-name">{item.Mfr_CommonName}</div>
+                                    <div className="country-name">{item.Country}</div>
+                                    <div className="type-name">{item?.VehicleTypes[0]?.Name}</div>
+                                </div>
+                            }
+                        </>
+                    )
+                })}
+
+                <div>
+                    <Modal className="popup-container" show={show} onHide={handleClose} centered>
+                        <Modal.Body className="modal-body">{ownerDetail.Country}
+                            <div className="ownerDetail-name">{ownerDetail.Mfr_Name}</div>
+                            <div className="owner-Ceo">{ownerDetail.PrincipalPositio}</div>
+                            <div className="owner-Address">{ownerDetail.Address}</div>
+                        </Modal.Body>
+                        <Modal.Footer className="footer">
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </div>
+
+
+            </section>
+
+
+        </>
+    )
 }
 
-export default Homepage;
+export default LandingPage;
